48c114fc07c6f7776325d31cdabce2612349c97f
Pratikkshirsagar <meetpratiik@gmail.com>
2020-05-10 01:56:46 +0530

    html added

--- app/Main.js
+++ app/Main.js
@@ -1,17 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function ExampleComponent() {
   return (
     <div>
       <h1>This is our app!!!</h1>
-      <p>The sky is blue and the grass is green.</p>
+      <p>The is blue and the grass is green.</p>
     </div>
-  )
+  );
 }
 
-ReactDOM.render(<ExampleComponent />, document.querySelector("#app"))
+ReactDOM.render(<ExampleComponent />, document.querySelector('#app'));
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
