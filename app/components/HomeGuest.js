2a4dad6ae1d0336d405efa72f73d1d13c09f18cc
Pratikkshirsagar <meetpratiik@gmail.com>
2020-07-08 13:09:31 +0530

    user registration done

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Page from './Page';
+import Axios from 'axios';
 
 function HomeGuest() {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      await Axios.post('http://localhost:8080/register', {
+        username,
+        email,
+        password,
+      });
+      console.log('user was successfully created');
+    } catch (e) {
+      console.log(e.response.data);
+    }
+  }
+
   return (
     <Page title="Welcome" wide={true}>
       <div className="row align-items-center">
@@ -15,7 +34,7 @@ function HomeGuest() {
           </p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="username-register" className="text-muted mb-1">
                 <small>Username</small>
@@ -27,6 +46,7 @@ function HomeGuest() {
                 type="text"
                 placeholder="Pick a username"
                 autoComplete="off"
+                onChange={(e) => setUsername(e.target.value)}
               />
             </div>
             <div className="form-group">
@@ -40,6 +60,7 @@ function HomeGuest() {
                 type="text"
                 placeholder="you@example.com"
                 autoComplete="off"
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="form-group">
@@ -52,6 +73,7 @@ function HomeGuest() {
                 className="form-control"
                 type="password"
                 placeholder="Create a password"
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <button
